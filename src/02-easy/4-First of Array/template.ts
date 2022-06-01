d3a81622efdf29b6c3ac993fd9c4e703ba5168bd
widewu <979610327@qq.com>
2022-06-01 22:29:08 +0800

    feat: update first of knowledge two method

--- src/02-easy/4-First of Array/template.ts
+++ src/02-easy/4-First of Array/template.ts
@@ -1,1 +1,4 @@
-type First<T extends any[]> = T extends [] ? never : T[0]
+// type First<T extends any[]> = T extends [] ? never : T[0]
+
+
+type First<T extends any[]> = T extends [infer P, ...infer reset] ? P : never
